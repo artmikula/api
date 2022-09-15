561441ada32e2ad8bf4beb12cf9d69e282893689
Arthur <arthurmikula@icloud.com>
2022-09-15 10:26:00 +0900

    model update

--- milkt_api/sensor/models.py
+++ milkt_api/sensor/models.py
@@ -1,5 +1,3 @@
-import datetime
-import logging
 from pickle import FALSE
 from django.db import models
 
@@ -11,41 +9,41 @@ class User(models.Model):
   pw = models.CharField(max_length=255)
   permission = models.IntegerField(max_length=1, choices=PERMISSIONS)
   is_deleted = models.BooleanField(default=FALSE)
-  create_time = models.DateTimeField(default=datetime.now(), blank=True)
-  update_time = models.DateTimeField(default=datetime.now(), blank=True)
-  delete_time = models.DateTimeField(default=datetime.now(), blank=True)
+  create_time = models.DateTimeField(auto_now=True)
+  update_time = models.DateTimeField(auto_now=True)
+  delete_time = models.DateTimeField(auto_now=True)
 
 class Farm(models.Model):
   farm_name = models.CharField(max_length=30)
   is_deleted = models.BooleanField(default=FALSE)
-  create_time = models.DateTimeField(default=datetime.now(), blank=True)
-  update_time = models.DateTimeField(default=datetime.now(), blank=True)
-  delete_time = models.DateTimeField(default=datetime.now(), blank=True)
+  create_time = models.DateTimeField(auto_now=True)
+  update_time = models.DateTimeField(auto_now=True)
+  delete_time = models.DateTimeField(auto_now=True)
 
 class Cow(models.Model):
   cow_id = models.CharField(max_length=255)
   farm_id = models.ForeignKey(Farm, on_delete=models.DO_NOTHING)
   is_deleted = models.BooleanField(default=FALSE)
-  create_time = models.DateTimeField(default=datetime.now(), blank=True)
-  update_time = models.DateTimeField(default=datetime.now(), blank=True)
-  delete_time = models.DateTimeField(default=datetime.now(), blank=True)
+  create_time = models.DateTimeField(auto_now=True)
+  update_time = models.DateTimeField(auto_now=True)
+  delete_time = models.DateTimeField(auto_now=True)
 
 class Sensor(models.Model):
   uid = models.CharField(max_length=255)
   owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
   ip_addr = models.CharField(max_length=255)
   is_deleted = models.BooleanField(default=FALSE)
-  create_time = models.DateTimeField(default=datetime.now(), blank=True)
-  update_time = models.DateTimeField(default=datetime.now(), blank=True)
-  delete_time = models.DateTimeField(default=datetime.now(), blank=True)
+  create_time = models.DateTimeField(auto_now=True)
+  update_time = models.DateTimeField(auto_now=True)
+  delete_time = models.DateTimeField(auto_now=True)
 
 class Cow_Sensor(models.Model):
   cow_id = models.ForeignKey(Cow, on_delete=models.DO_NOTHING)
   sensor_id = models.ForeignKey(Sensor, on_delete=models.DO_NOTHING)
   is_deleted = models.BooleanField(default=FALSE)
-  create_time = models.DateTimeField(default=datetime.now(), blank=True)
-  update_time = models.DateTimeField(default=datetime.now(), blank=True)
-  delete_time = models.DateTimeField(default=datetime.now(), blank=True)
+  create_time = models.DateTimeField(auto_now=True)
+  update_time = models.DateTimeField(auto_now=True)
+  delete_time = models.DateTimeField(auto_now=True)
 
 class Sensor_Data(models.Model):
   sensor_id = models.ForeignKey(Sensor, on_delete=models.DO_NOTHING)
@@ -57,11 +55,11 @@ class Sensor_Data(models.Model):
   Gyro_Y = models.IntegerField
   Gyro_Z = models.IntegerField
   temperature = models.IntegerField
-  rcv_time = models.DateTimeField(default=datetime.now(), blank=True)
+  rcv_time = models.DateTimeField(auto_now=True)
   is_deleted = models.BooleanField(default=FALSE)
-  create_time = models.DateTimeField(default=datetime.now(), blank=True)
-  update_time = models.DateTimeField(default=datetime.now(), blank=True)
-  delete_time = models.DateTimeField(default=datetime.now(), blank=True)
+  create_time = models.DateTimeField(auto_now=True)
+  update_time = models.DateTimeField(auto_now=True)
+  delete_time = models.DateTimeField(auto_now=True)
 
 class Command(models.Model):
   COMMAND_STATUS = (
@@ -88,9 +86,9 @@ class Command(models.Model):
   command_status = models.IntegerField(max_length=1, choices=COMMAND_STATUS)
   rcv_code = models.IntegerField
   is_deleted = models.BooleanField(default=FALSE)
-  create_time = models.DateTimeField(default=datetime.now(), blank=True)
-  update_time = models.DateTimeField(default=datetime.now(), blank=True)
-  delete_time = models.DateTimeField(default=datetime.now(), blank=True)
+  create_time = models.DateTimeField(auto_now=True)
+  update_time = models.DateTimeField(auto_now=True)
+  delete_time = models.DateTimeField(auto_now=True)
 
 class Log(models.Model):
   DIV = (
@@ -104,12 +102,12 @@ class Log(models.Model):
   )
   div = models.IntegerField(max_length=1, choices=DIV)
   command_id = models.ForeignKey(Command, on_delete=models.DO_NOTHING)
-  sensor_uid = models.IntegerField
-  sensor_date_id = models.ForeignKey(Sensor_Data, on_delete=models.DO_NOTHING)
+  sensor_uid = models.ForeignKey(Sensor, on_delete=models.DO_NOTHING)
+  sensor_data_id = models.ForeignKey(Sensor_Data, on_delete=models.DO_NOTHING)
   logging_type = models.IntegerField(max_length=1, choices=LOGGING_TYPE)
   response_code = models.IntegerField
   logging_msg = models.CharField(max_length=255)
   is_deleted = models.BooleanField(default=FALSE)
-  create_time = models.DateTimeField(default=datetime.now(), blank=True)
-  update_time = models.DateTimeField(default=datetime.now(), blank=True)
-  delete_time = models.DateTimeField(default=datetime.now(), blank=True)
+  create_time = models.DateTimeField(auto_now=True)
+  update_time = models.DateTimeField(auto_now=True)
+  delete_time = models.DateTimeField(auto_now=True)
